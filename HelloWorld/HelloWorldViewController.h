e22f00f54ffe44f8b527632f92618f431009f3c1
Eric Meyer <emeyer@8thlight.com>
2011-12-15 20:57:56 -0600

    Initial Commit

---
+++ HelloWorld/HelloWorldViewController.h
@@ -0,0 +1,15 @@
+//
+//  HelloWorldViewController.h
+//  HelloWorld
+//
+//  Created by Eric Meyer on 12/15/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldViewController : UIViewController {
+    
+}
+
+@end
---
+++ HelloWorldTests/HelloWorldTests.h
@@ -0,0 +1,17 @@
+//
+//  HelloWorldTests.h
+//  HelloWorldTests
+//
+//  Created by Eric Meyer on 12/15/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface HelloWorldTests : SenTestCase {
+@private
+    
+}
+
+@end
